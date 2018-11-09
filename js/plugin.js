b3b0c54094a08c9433715a9fca890631785fc3cc
Blayne <roman.u.marchenko@gmail.com>
2018-11-09 16:14:26 +0200

    testing timer

--- js/plugin.js
+++ js/plugin.js
@@ -17,15 +17,46 @@ const timer = (function () {
     }
 
     function start(seconds) {
+        if (!timerDisplay || !endTime) return console.log('Please init module fist!');
+        if (!seconds || typeof seconds !== "number") return console.log('Please choose mod!');
 
+        clearInterval(countdown);
+        alarmSound.pause();
+
+        const now = Date.now();
+        const then = now + seconds * 1000;
+
+        displayTimeLeft(seconds);
+        displayEndTime(then);
+
+        countdown = setInterval(() => {
+            const secondsLeft = Math.round((then - Date.now()) / 1000);
+
+            if (secondsLeft < 0) {
+                clearInterval(countdown);
+                playSound();
+                return;
+            }
+
+            displayTimeLeft(secondsLeft);
+        }, 1000);
     }
 
     function displayTimeLeft(seconds) {
+        const minutes = Math.floor(seconds / 60);
+        const reminderSeconds = Math.floor(seconds % 60);
 
+        const display = `${minutes}:${reminderSeconds < 10 ? '0' : ''}${reminderSeconds} `;
+        document.title = display;
+        timerDisplay.textContent = display;
     }
 
     function displayEndTime(timestamp) {
+        const end = new Date(timestamp);
+        const hour = end.getHours();
+        const minutes = end.getMinutes();
 
+        endTime.textContent = `Be back at ${hour}:${minutes < 10 ? '0' : ''}${minutes} `;
     }
 
     function stop() {
@@ -33,7 +64,8 @@ const timer = (function () {
     }
 
     function playSound() {
-
+        alarmSound.load();
+        alarmSound.play();
     }
 
     return {
@@ -45,7 +77,7 @@ const timer = (function () {
 }());
 
 timer.init({
-    timeLeftSelector: '.display__timer-left',
+    timeLeftSelector: '.display__time-left',
     timeEndSelector: '.display__end-time',
     alarmSound: 'audio/Serbsky.mp3'
 });
@@ -55,4 +87,6 @@ function startTimer(e) {
     timer.start(seconds);
 }
 
+let custom = document.forms['customForm'].elements['minutes'].value;
+console.log(custom);
 buttons.forEach(btn => btn.addEventListener('click', startTimer));
